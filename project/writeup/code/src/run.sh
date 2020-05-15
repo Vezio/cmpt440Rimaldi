dacb86b4a5c953859726997e14513159aaa28fce
vezio <tyler.rimaldi@ibm.com>
2020-05-15 04:40:47 -0400

    edited test cases

--- project/writeup/code/src/run.sh
+++ project/writeup/code/src/run.sh
@@ -36,11 +36,3 @@ echo "RUNNING TEST CASE 3: "
 echo "=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~"
 java Driver -f ../../tests/test_case_3.txt
 
-# echo "RUNNING TEST CASE 4: "
-# java Driver -f ../../data/test_case4.txt
-
-# echo "RUNNING TEST CASE 5: "
-# java Driver -f ../../data/test_case5.txt
-
-# echo "RUNNING TEST CASE 6: "
-# java Driver -f ../../data/test_case6.txt
